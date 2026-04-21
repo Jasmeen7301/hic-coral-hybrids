0a1f6a7150649f016555274cee2f58a8ffbe3880
Jasmeen7301 <kaur.jasmeen1106@gmail.com>
2026-04-21 23:02:49 +1000

    Update input file for chrom sizes generation

--- 03_create_chrom_sizes.sh
+++ 03_create_chrom_sizes.sh
@@ -1,2 +1,2 @@
-awk 'BEGIN{OFS="\t"}{print $1, $NF}' restriction_sites/2561_hap1_clean.fasta_Arima.txt > chrom_sizes_2561_hap1
+awk 'BEGIN{OFS="\t"}{print $1, $NF}' restriction_sites/sample_hap1_clean.fasta_Arima.txt > chrom_sizes_sample_hap1
 
